d2db03f926ebd0355fe78b1aa6d2a60f49c627d3
ariibaguss <125134103+ariibaguss@users.noreply.github.com>
2023-11-04 11:02:19 +0700

    third commit

--- app/src/test/java/com/example/junit_testing/CalculatorTest.kt
+++ app/src/test/java/com/example/junit_testing/CalculatorTest.kt
@@ -1,15 +1,22 @@
 package com.example.junit_testing
 
+ // Mengimpor kelas Calculator dari package com.example.junit_test
 import com.example.junit_test.Calculator
 import org.junit.Test
+// Mengimpor fungsi assertEquals dari package kotlin.test
 import kotlin.test.assertEquals
 
+ // Mendefinisikan kelas CalculatorTest
 class CalculatorTest {
 
+    // Membuat objek dari kelas Calculator
     val calculator = Calculator()
 
     @Test
+    // Mendefinisikan metode pengujian
     fun `Test calculator can subtract two numbers`() {
+        // Memanggil fungsi assertEquals untuk membandingkan hasil perhitungan 
+        // dengan nilai yang diharapkan
         assertEquals(10, calculator.sub(20, 10))
     }
 }
