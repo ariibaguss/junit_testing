658336bfe2b8096e4e848ba5b4d2f140c265a6fc
ariibaguss <125134103+ariibaguss@users.noreply.github.com>
2023-11-04 11:03:54 +0700

    third commit

--- app/src/main/java/com/example/junit_testing/Calculator.kt
+++ app/src/main/java/com/example/junit_testing/Calculator.kt
@@ -1,9 +1,14 @@
 package com.example.junit_test
 
+// Mendefinisikan kelas Calculator
 class Calculator {
+     // Mendefinisikan metode untuk penjumlahan
     fun add(first: Int, second: Int): Int {
+        // Melakukan penjumlahan antara 'second' dan 'first', 
+        // dan mengembalikan hasilnya
         return second + first
     }
+     // Mendefinisikan metode untuk pengurangan
     fun sub(first: Int, second: Int): Int {
             return first - second
     }
